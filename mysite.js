c2ff957aa12ef07da89ea005bdee7d093588b00e
Subin67 <rs4188832@gmail.com>
2026-01-20 13:18:45 +0700

    mysite.js

--- mysite.js
+++ mysite.js
@@ -1,13 +1,10 @@
 function showInfo() {
-  // Get form values
   const name = document.getElementById("name").value || "N/A";
   const email = document.getElementById("email").value || "N/A";
   const message = document.getElementById("message").value || "N/A";
 
-  // Get the container
   const infoDiv = document.getElementById("userInfo");
 
-  // Display the info
   infoDiv.style.display = "block";
   infoDiv.innerHTML = `
     <h4>Your Information:</h4>
